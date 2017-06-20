8a023edafc2534dc833a98d7d5c3093b24e55538
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2017-06-20 09:13:24 +0800

    add PHP variables

---
+++ variables.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>Variables</title>
+	</head>
+	<body>
+		<?php 
+		$num=15.56;
+		$var=95;
+		$str="What's up?";
+		echo "{$num}";
+		echo "<br />";
+		echo $var;
+		echo "<br />";
+		echo $str;
+		?>
+	</body>
+</html>
