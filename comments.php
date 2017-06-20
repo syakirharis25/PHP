5d7aa28e3cd497f0b2911628817b664ac3a21c80
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2017-06-20 09:00:30 +0800

    add PHP comment

---
+++ comments.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Comments in PHP</title>
+	</head>
+	<body>
+		<?php 	//This is a single line comment
+				/*This is a multi-line
+				comment. You may write as 
+				much as you wish to write*/?>
+		<?php echo "Commenting code in PHP"; ?>
+	</body>
+</html>
